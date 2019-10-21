a282082c54457929d0ee7000c2da481353de47d7
Yana_Z <yvz1973@gmail.com>
2019-10-21 16:16:46 +0100

    adds def setup for array library

--- library_class.rb
+++ library_class.rb
@@ -1,30 +1,6 @@
 class Library
 
 #create a class for a library that has an array of books
-  # books_library = [
-  #
-  #   { title: "war_and_peace",
-  #     rental_details: {
-  #       student_name: "Yana",
-  #       date: "10/11/20"
-  #     }
-  #   },
-  #
-  #   { title: "the_pearl",
-  #     rental_details: {
-  #       student_name: "Marc",
-  #       date: "20/11/20"
-  #     }
-  #
-  #   },
-  #
-  #   { title: "h2g2",
-  #     rental_details: {
-  #       student_name: "Hitch",
-  #       date: "14/05/20"
-  #       }
-  #   }
-  # ]
 
 
 def initialize (books_array)
@@ -42,7 +18,6 @@ end
   def get_book(book_num)
     return @books_array[book_num]
   end
-
 
 
 end
--- specs/library_class_spec.rb
+++ specs/library_class_spec.rb
@@ -4,13 +4,46 @@ require_relative('../library_class.rb')
 
 class TestLibraryClass < MiniTest::Test
 
+def setup
+  @books_list =
+  [
+
+    { title: "war_and_peace",
+      rental_details: {
+        student_name: "Yana",
+        date: "10/11/20"
+      }
+    },
+
+    { title: "the_pearl",
+      rental_details: {
+        student_name: "Marc",
+        date: "20/11/20"
+      }
+
+    },
+
+    { title: "h2g2",
+      rental_details: {
+        student_name: "Hitch",
+        date: "14/05/20"
+        }
+    }
+  ]
+end
 
 
 def test_get_book
-  books_list = ["book1","book2","book3"]
-  library_class = Library.new(books_list)
+
+  library_class = Library.new(@books_list)
   result = library_class.get_book(1)
-  assert_equal("book2", result)
+  assert_equal({ title: "the_pearl",
+    rental_details: {
+      student_name: "Marc",
+      date: "20/11/20"
+    }
+
+  }, result)
 end
 
 
