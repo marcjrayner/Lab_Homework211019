577418dd0fa63c763429326b0d8067fd22ebc922
Yana_Z <yvz1973@gmail.com>
2019-10-21 15:44:29 +0100

    adds library class docs, including books array

---
+++ library_class.rb
@@ -0,0 +1,33 @@
+class Library
+
+books = [
+
+  { title: "war_and_peace",
+    rental_details: {
+      student_name: "Yana",
+      date: "10/11/20"
+    }
+  },
+
+  { title: "the_pearl",
+    rental_details: {
+      student_name: "Marc",
+      date: "20/11/20"
+    }
+
+  },
+
+  { title: "h2g2",
+    rental_details: {
+      student_name: "Hitch",
+      date: "14/05/20"
+      }
+  }
+]
+
+
+
+
+  ]
+
+end
---
+++ specs/library_class.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../library_class.rb')
+
+class TestLibraryClass < MiniTest::Test
+
+
+
+
+
+
+
+
+
+
+end
