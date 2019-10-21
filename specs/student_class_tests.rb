5653af4de0172bfcc2ed9f9001439af84d7361dc
Yana_Z <yvz1973@gmail.com>
2019-10-21 13:39:36 +0100

    third test pass

--- specs/student_class_tests.rb
+++ specs/student_class_tests.rb
@@ -6,14 +6,21 @@ require_relative('../student_class.rb')
 class TestStudentsClass < MiniTest::Test
 
 
-def test_student_class_name
-  student_class = Student.new("Marc", "e35")
-  assert_equal("Marc", student_class.get_name)
-end
+  def test_student_class_name
+    student_class = Student.new("Marc", "e35")
+    assert_equal("Marc", student_class.get_name)
+  end
 
-def test_student_class_cohort
-  student_class = Student.new("Richard", "e35")
-  assert_equal("e35", student_class.get_cohort)
-end
+  def test_student_class_cohort
+    student_class = Student.new("Richard", "e35")
+    assert_equal("e35", student_class.get_cohort)
+  end
+
+  def test_student_class_set_name
+    student_class = Student.new("Yana", "e35")
+    student_class.set_name("Jim")
+    assert_equal("Jim",student_class.get_name)
+  end
+
 
 end
--- student_class.rb
+++ student_class.rb
@@ -4,10 +4,17 @@ def initialize(name, cohort)
     @name = name
     @cohort = cohort
   end
+
   def get_name
     return @name
   end
+
   def get_cohort
     return @cohort
   end
+
+  def set_name(new_name)
+    @name = new_name
+  end
+
 end
